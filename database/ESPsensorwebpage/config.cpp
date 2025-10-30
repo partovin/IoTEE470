0ef2ff358ea718cb02a84380a510549a8a64eccb
partovin <161419145+partovin@users.noreply.github.com>
2025-10-30 12:01:26 -0700

    Create config.cpp

---
+++ database/ESPsensorwebpage/config.cpp
@@ -0,0 +1,7 @@
+// config.cpp
+#include "config.h"
+
+// Put your real credentials here
+const char* WIFI_SSID     = "iPhone (64)";
+const char* WIFI_PASSWORD = "*************";
+const char* BASE_URL      = "https://nimapartovi.org/index.php";
